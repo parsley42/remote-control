691d33e7ff2bf57f278aac03df9eef12ec7d73ee
David Parsley <parsley@linuxjedi.org>
2016-03-29 16:23:23 -0400

    Add -s (semi-silent) flag to yumcheck

--- defaultsite/taskscripts/yumcheck.sh
+++ defaultsite/taskscripts/yumcheck.sh
@@ -4,15 +4,19 @@
 
 usage(){
 	cat <<EOF
-Usage: yumcheck.sh
+Usage: yumcheck.sh (-h) (-s)
 
 Check to see if updates are needed. Non-zero return -> updates are needed.
+-s - only output whether the host needs updates or not
 EOF
 	exit 1
 }
 
 [ "$1" = "-h"  ] && usage
 
-echo "*** $(date) - Checking whether updates are needed."
+[ -z "$1" ] && echo "*** $(date) - Checking whether updates are needed."
 
 yum check-update
+RETVAL=$?
+[ -n "$1" -a $RETVAL -eq 100 ] && echo "needs updates"
+exit $RETVAL

63f307589d22b1045d138f11db4c10b78ec68540
David L. Parsley <parsley@linuxjedi.org>
2016-03-29 07:42:51 -0400

    Refactor hostlib, don't use subshells

--- libslocal/hostlib.sh
+++ libslocal/hostlib.sh
@@ -1,83 +1,86 @@
 # Look up members of a host group and echo space-separated string
 
 resolvehostgroup(){
-	local HOSTGROUP=${1#@}
-	local HOST REST
+	functrap
+	local HOSTGROUP HOST REST HOSTLIST
+	HOSTGROUP=${1#@}
 	HOSTFILE="$RCROOT/sites/$RCSITE/hostgroups/${HOSTGROUP}.hosts"
-	[ ! -e "$HOSTFILE" ] && { errormsg "Hosts file $RCROOT/sites/$RCSITE/hostgroups/${HOSTGROUP}.hosts doesn't exist"; return 1; }
+	[ ! -e "$HOSTFILE" ] && { errormsg "Hosts file $RCROOT/sites/$RCSITE/hostgroups/${HOSTGROUP}.hosts doesn't exist"; return 0; }
 	while read HOST REST
 	do
 		[[ $HOST = \#* ]] && continue
-		echo -n "$HOST "
+		HOSTLIST="$HOSTLIST $HOST"
 	done < $HOSTFILE
-	echo
+	addhosts $HOSTLIST
 }
 
 # Look up a host alias in hostgroups/* files
 resolvehostalias(){
-	local MATCHHOST=$1
-	local HOSTMATCH=$(grep -Rh "\<$MATCHHOST\>" "$RCROOT/sites/$RCSITE/hostgroups" | sort | uniq)
-	local HOSTLINES=$(echo "$HOSTMATCH" | wc -l)
+	functrap
+	local MATCHHOST HOSTMATCH HOSTLINES
+	MATCHHOST=$1
+	HOSTLINES=0
+	if [ -n "$(ls -A "$RCROOT/sites/$RCSITE/hostgroups/" 2>/dev/null)" ]
+	then
+		HOSTMATCH=$(grep -Rh "\<$MATCHHOST\>" "$RCROOT/sites/$RCSITE/hostgroups" | sort | uniq)
+		HOSTLINES=$(echo "$HOSTMATCH" | wc -l)
+	fi
 	if [ -n "$HOSTMATCH" -a $HOSTLINES -eq 1 ]
 	then
-		echo ${HOSTMATCH%% *}
+		addhosts ${HOSTMATCH%% *}
 		return 0
 	fi
-	[ $HOSTLINES -gt 1 ] && { errormsg "Host/alias $MATCHHOST found in multiple non-matching host lines: $HOSTMATCH"; return 1; }
-	echo $MATCHHOST
+	[ $HOSTLINES -gt 1 ] && { errormsg "Host/alias $MATCHHOST found in multiple non-matching host lines: $HOSTMATCH"; return 0; }
+	addhosts $MATCHHOST
 }
 
 hostnicename(){
-	local MATCHHOST=${1}
-	[ ! -d "$RCROOT/sites/$RCSITE/hostgroups" ] && { echo $MATCHHOST; return 0; }
+	functrap
+	local MATCHHOST=$1
+	[ ! -d "$RCROOT/sites/$RCSITE/hostgroups" ] && { RCNICENAME=$MATCHHOST; return 0; }
 	if [ -n "$(ls -A "$RCROOT/sites/$RCSITE/hostgroups/")" ]
 	then
 		local HOSTMATCH=$(grep -Rh "^\<$MATCHHOST\>" "$RCROOT/sites/$RCSITE/hostgroups" | sort | uniq)
 		local HOSTLINES=$(echo "$HOSTMATCH" | wc -l)
 		if [ $HOSTLINES -eq 1 -a -n "$HOSTMATCH" ]
 		then
-			echo ${HOSTMATCH##* }
+			RCNICENAME=${HOSTMATCH##* }
 		else
-			echo $MATCHHOST
+			RCNICENAME=$MATCHHOST
 		fi
-		[ $HOSTLINES -gt 1 ] && { errormsg "Host/alias $MATCHHOST found in multiple non-matching host lines: $HOSTMATCH"; return 1; }
+		[ $HOSTLINES -gt 1 ] && { errormsg "Host/alias $MATCHHOST found in multiple non-matching host lines: $HOSTMATCH"; return 0; }
 	else
-		echo $MATCHHOST
+		RCNICENAME=$MATCHHOST
 	fi
 	return 0
 }
 
 # Add hosts to a list: addhosts varname <host> ...
 addhosts(){
+	functrap
 	local ADDHOST
-	local LISTNAME=$1
-	local HOSTLIST=${!1}
 
-	shift
 	for ADDHOST in $*
 	do
-		echo " $HOSTLIST " | grep -q " $ADDHOST " || HOSTLIST="$HOSTLIST $ADDHOST"
+		echo " $RCREMOTEHOSTS " | grep -q " $ADDHOST " || RCREMOTEHOSTS="$RCREMOTEHOSTS $ADDHOST"
 	done
-	HOSTLIST=${HOSTLIST# }
-	eval $LISTNAME=\"$HOSTLIST\"
+	RCREMOTEHOSTS=${RCREMOTEHOSTS# }
 }
 
 resolvehostlist(){
-	local HOSTSPEC LIBHOSTSHOSTLIST HOSTARRAY RESHOST RESGROUP
+	functrap
+	local HOSTSPEC HOSTARRAY
 	for HOSTSPEC in $*
 	do
 		case $HOSTSPEC in
 			@*)
-				RESGROUP=$(resolvehostgroup ${HOSTSPEC}) || return 1
-				addhosts LIBHOSTSHOSTLIST $RESGROUP
+				resolvehostgroup ${HOSTSPEC}
 				;;
 			*)
-				RESHOST=$(resolvehostalias ${HOSTSPEC}) || return 1
-				addhosts LIBHOSTSHOSTLIST $RESHOST
+				resolvehostalias ${HOSTSPEC}
 				;;
 		esac
 	done
-	HOSTARRAY=($LIBHOSTSHOSTLIST)
-	[ ${#HOSTARRAY[@]} -eq 0 ] && { errormsg "Couldn't resolve any hosts in: $*"; return 1; }
-	echo $LIBHOSTSHOSTLIST
+	HOSTARRAY=($RCREMOTEHOSTS)
+	[ ${#HOSTARRAY[@]} -eq 0 ] && { errormsg "Couldn't resolve any hosts in: $*"; return 0; } || :
 }

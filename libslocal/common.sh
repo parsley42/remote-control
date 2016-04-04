fb6681e9bba0e5466bf2c94368af966516ed568b
David L. Parsley <parsley@linuxjedi.org>
2016-04-04 15:59:27 -0400

    Start rework of defs files & locations

--- libslocal/common.sh
+++ libslocal/common.sh
@@ -5,9 +5,9 @@ function status(){
 	echo -e "\n### $(date) - $1"
 }
 
-# catdefs cat's out all the definitions files relevant to a task.
+# taskdefs cat's out all the definitions files relevant to a task.
 # Each successive definitions file can override a previous.
-catdefs (){
+taskdefs (){
 	local RCHOST=$1
 	local RCTASK=$2
 	# If there's a site.defs file, add it in
@@ -15,13 +15,13 @@ catdefs (){
 	# Same for a host .defs file
 	[ -e "$RCROOT/sites/$RCSITE/hosts/$RCHOST/host.defs" ] && cat "$RCROOT/sites/$RCSITE/hosts/$RCHOST/host.defs" || :
 	# First, any definitions from defaultsite
-	[ -e "$RCROOT/defaultsite/defs/${RCTASK}.defs" ] && cat "$RCROOT/defaultsite/defs/${RCTASK}.defs" || :
+	[ -e "$RCROOT/defaultsite/taskdefs/${RCTASK}.defs" ] && cat "$RCROOT/defaultsite/taskdefs/${RCTASK}.defs" || :
 	# ... then definitions from sites/common
-	[ -e "$RCROOT/sites/common/defs/${RCTASK}.defs" ] && cat "$RCROOT/sites/common/defs/${RCTASK}.defs" || :
+	[ -e "$RCROOT/sites/common/taskdefs/${RCTASK}.defs" ] && cat "$RCROOT/sites/common/taskdefs/${RCTASK}.defs" || :
 	# Now check for site task definitions
-	[ -e "$RCROOT/sites/$RCSITE/defs/${RCTASK}.defs" ] && cat "$RCROOT/sites/$RCSITE/defs/${RCTASK}.defs" || :
+	[ -e "$RCROOT/sites/$RCSITE/taskdefs/${RCTASK}.defs" ] && cat "$RCROOT/sites/$RCSITE/taskdefs/${RCTASK}.defs" || :
 	# ... and host task definitions
-	[ -e "$RCROOT/sites/$RCSITE/hosts/$RCHOST/defs/${RCTASK}.defs" ] && cat "$RCROOT/sites/$RCSITE/hosts/$RCHOST/defs/${RCTASK}.defs" || :
+	[ -e "$RCROOT/sites/$RCSITE/hosts/$RCHOST/taskdefs/${RCTASK}.defs" ] && cat "$RCROOT/sites/$RCSITE/hosts/$RCHOST/taskdefs/${RCTASK}.defs" || :
 	# Finally, if an RCDEFSFILE argument was passed on the command line, it overrides everything
 	[ -n "$RCDEFSFILE" ] && cat "$RCDEFSFILE" || :
 }

3a9f7b30e26c1aeeed50df40fdd4edbb4b315dea
David L. Parsley <parsley@linuxjedi.org>
2016-04-05 12:24:02 -0400

    Work work work... (in progress)

--- libslocal/errhandle.sh
+++ libslocal/errhandle.sh
@@ -5,15 +5,20 @@
 error_handler(){
     echo "$0 exiting on error: \"$1\" at line $2, exit code $3." >&2
 }
-alias errtrap="trap 'error_handler \"\${BASH_COMMAND}\" \$LINENO \$?' ERR"
-errtrap
+trap 'error_handler \"${BASH_COMMAND}\" $LINENO $?' ERR
 
 # Functions can use the functrap alias to set up this trap; note
 # that it's not useful in a subshell
 func_error_handler(){
 	echo "$0 exiting on error in function $1: \"$2\" at line $3, exit code $4." >&2
 }
-alias functrap="trap 'func_error_handler \${FUNCNAME[0]} \"\${BASH_COMMAND}\" \$LINENO \$?' ERR"
+#functrap:trap 'func_error_handler ${FUNCNAME[0]} \"${BASH_COMMAND}\" $LINENO $?' ERR
+
+# Since jobs are cat'ed to /bin/bash, we want useful error messages
+job_error_handler(){
+	echo "$0 exiting on error in job $1: \"$2\" at line $3, exit code $4." >&2
+}
+#jobtrap:trap 'job_error_handler ${RCJOBSCRIPT} \"${BASH_COMMAND}\" $((LINENO - RCFIRSTLINE)) $?' ERR
 
 # Print error to stderr and exit, for checked errors
 errorout(){
--- libslocal/tasklib.sh
+++ libslocal/tasklib.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
-# taskinfo.sh - get information about tasks
+# tasklib.sh - rc task functions
+
+# taskdefs cat's out all the definitions files relevant to a task.
+# Each successive definitions file can override a previous.
+taskdefs (){
+	local RCHOST=$1
+	local RCTASK=$2
+	# If there's a site.defs file, add it in
+	[ -e "$RCROOT/sites/$RCSITE/site.defs" ] && cat "$RCROOT/sites/$RCSITE/site.defs" || :
+	# Same for a host .defs file
+	[ -e "$RCROOT/sites/$RCSITE/hosts/$RCHOST/host.defs" ] && cat "$RCROOT/sites/$RCSITE/hosts/$RCHOST/host.defs" || :
+	# First, any definitions from defaultsite
+	[ -e "$RCROOT/defaultsite/taskdefs/${RCTASK}.defs" ] && cat "$RCROOT/defaultsite/taskdefs/${RCTASK}.defs" || :
+	# ... then definitions from sites/common
+	[ -e "$RCROOT/sites/common/taskdefs/${RCTASK}.defs" ] && cat "$RCROOT/sites/common/taskdefs/${RCTASK}.defs" || :
+	# Now check for site task definitions
+	[ -e "$RCROOT/sites/$RCSITE/taskdefs/${RCTASK}.defs" ] && cat "$RCROOT/sites/$RCSITE/taskdefs/${RCTASK}.defs" || :
+	# ... and host task definitions
+	[ -e "$RCROOT/sites/$RCSITE/hosts/$RCHOST/taskdefs/${RCTASK}.defs" ] && cat "$RCROOT/sites/$RCSITE/hosts/$RCHOST/taskdefs/${RCTASK}.defs" || :
+	# Finally, if an RCDEFSFILE argument was passed on the command line, it overrides everything
+	[ -n "$RCDEFSFILE" ] && cat "$RCDEFSFILE" || :
+}
 
 # List all site tasks for list command
 listsitetasks(){

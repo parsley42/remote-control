29aca0a49d51791c19d99d6e1b53073e7794a1e1
David L. Parsley <parsley@linuxjedi.org>
2017-04-11 13:35:40 -0400

    Add -q - quiet option to suppress stderr messages

--- libslocal/common.sh
+++ libslocal/common.sh
@@ -5,6 +5,13 @@ function status(){
 	echo -e "\n### $(date) - $1"
 }
 
+printout(){
+	if [ -z "$RCQUIET" ]
+	then
+		echo "$1" >&2
+	fi
+}
+
 generateid(){
 	echo $(dd if=/dev/urandom bs=16 count=1 2>/dev/null | md5sum | cut -f 1 -d' ')
 }
